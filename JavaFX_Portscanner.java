7efa543c5485a065fcea3f797af469e3a96aa8ae
Martin Tastler <123328309+Martin2p@users.noreply.github.com>
2025-05-26 21:36:41 +0200

    Update JavaFX_Portscanner.java

--- JavaFX_Portscanner.java
+++ JavaFX_Portscanner.java
@@ -1,1 +1,44 @@
 package src;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class JavaFX_Portscanner extends Application {
+
+
+	@Override
+	public void start(Stage meineBuehne) throws Exception {
+			
+		//eine Instanz von FXMLLoader erzeugen
+		FXMLLoader portscanner = new FXMLLoader(getClass().getResource("Portscanner.fxml"));
+			
+		//die Datei laden
+		Parent root = portscanner.load();
+			
+		//Kontroller beschaffen
+		FXMLController trController = portscanner.getController();
+			
+		//und die Bühne übergeben
+		trController.setMeineStage(meineBuehne);
+			
+		//Scene erstellen
+		Scene meineScene = new Scene(root, 500, 560);
+			
+		//den Titel über stage setzen
+		meineBuehne.setTitle("Portscanner");
+		//die Szene setzen
+		meineBuehne.setScene(meineScene);
+		//Fenstergröße fix setzen
+		meineBuehne.setResizable(false);
+		//anzeigen
+		meineBuehne.show();
+	}
+		
+	public static void main(String[] args) {
+		//der Start
+		launch(args);
+	}
+}
