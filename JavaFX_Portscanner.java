42284c7485c7455cc38d1d6943d438d8ec848011
Martin Tastler <123328309+Martin2p@users.noreply.github.com>
2025-05-27 19:50:42 +0200

    Update JavaFX_Portscanner.java

--- JavaFX_Portscanner.java
+++ JavaFX_Portscanner.java
@@ -1,44 +1,44 @@
 package src;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class JavaFX_Portscanner extends Application {
 
-
-	@Override
-	public void start(Stage meineBuehne) throws Exception {
-			
-		//eine Instanz von FXMLLoader erzeugen
-		FXMLLoader portscanner = new FXMLLoader(getClass().getResource("Portscanner.fxml"));
-			
-		//die Datei laden
-		Parent root = portscanner.load();
-			
-		//Kontroller beschaffen
-		FXMLController trController = portscanner.getController();
-			
-		//und die Bühne übergeben
-		trController.setMeineStage(meineBuehne);
-			
-		//Scene erstellen
-		Scene meineScene = new Scene(root, 500, 560);
-			
-		//den Titel über stage setzen
-		meineBuehne.setTitle("Portscanner");
-		//die Szene setzen
-		meineBuehne.setScene(meineScene);
-		//Fenstergröße fix setzen
-		meineBuehne.setResizable(false);
-		//anzeigen
-		meineBuehne.show();
-	}
+public class FXMLController {
+	
+	/*
+	 * Declarations
+	*/
+	
+	@FXML private Stage myStage;
+	@FXML private Label outputIP;
+	@FXML private Label output;
+	
+	
+	/*
+	 * Methods
+	*/
 		
-	public static void main(String[] args) {
-		//der Start
-		launch(args);
-	}
+		//Method for setting the stage
+		public void setMyStage(Stage myStage) {
+			this.myStage = myStage;
+		}
+		
+		//Method for closing the program
+		@FXML protected void closeClick(ActionEvent event) {
+			Platform.exit();
+		}
+		
+		//Method for Software-Info
+		@FXML protected void infoClick(ActionEvent event) {
+			Alert info = new Alert(AlertType.INFORMATION, "From Martin Tastler");
+			info.setHeaderText("Portscanner Version Alpha 1");
+			info.show();
+		}
 }
+	
